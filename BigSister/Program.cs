577125b773bcf166b1939633d2b163b74e1493d6
Filandrexa <sacker@abv.bg>
2021-12-13 16:00:04 +0200

    Add project files.

---
+++ BigSister/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BigSister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
