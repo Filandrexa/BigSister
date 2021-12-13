6a3cd091f3085a17d4e8ac11c6b143049d3b0154
Filandrexa <sacker@abv.bg>
2021-12-13 16:32:12 +0200

    Happy path

--- BigSister/Program.cs
+++ BigSister/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace BigSister
 {
@@ -6,7 +7,31 @@ namespace BigSister
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //spaceship type, year of purchase, year of tax calculation, and light miles traveled
+
+            var input = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            var spaceShip = input[0].ToLower();
+            var yearOfPurchase = int.Parse(input[1]);
+            var yearOfTax = int.Parse(input[2]);
+            var lightMiles = int.Parse(input[3].Split('_', StringSplitOptions.RemoveEmptyEntries).ToArray()[0]);
+
+            var tax = 0;
+            if (spaceShip == "cargo")
+            {
+                tax += 10000;
+                tax += 1000 * lightMiles;
+                tax -= 736 * (yearOfTax - yearOfPurchase);
+            }
+            else if (spaceShip == "family")
+            {
+                tax += 5000;
+                tax += 100 * lightMiles;
+                tax -= 355 * (yearOfTax - yearOfPurchase);
+            }
+
+            Console.WriteLine($"Ship: {spaceShip}, purchased: {yearOfPurchase}, tax: {yearOfTax}, miles: {lightMiles}");
+            Console.WriteLine($"Tax to be payed:{tax} DVS");
         }
     }
 }
